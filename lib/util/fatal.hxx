162f0b9792b4bdcb917e83ee393639672afba9c6
L-LYR <lisicen97@gmail.com>
2024-10-19 12:47:03 +0800

    util: add source_location

--- lib/util/fatal.hxx
+++ lib/util/fatal.hxx
@@ -1,12 +1,22 @@
 #pragma once
+
 #include <format>
+#include <source_location>
+
+namespace detail {
+inline std::string die_prefix(std::source_location location) {
+  return std::format("{}:{} `{}`: ", location.file_name(), location.line(), location.function_name());
+}
+}  // namespace detail
 
 template <typename... Args>
-inline void die(std::string_view fmt, Args &&...args) {
-  throw std::runtime_error(std::vformat(fmt, std::make_format_args(args...)));
+inline void die(std::string_view fmt, std::source_location location, Args &&...args) {
+  throw std::runtime_error(detail::die_prefix(location) + std::vformat(fmt, std::make_format_args(args...)));
 }
 
 template <>
-inline void die<>(std::string_view message) {
-  throw std::runtime_error(message.data());
+inline void die<>(std::string_view message, std::source_location location) {
+  die("{}", location, message);
 }
+
+#define die(fmt, ...) die(fmt, std::source_location::current() __VA_OPT__(, ) __VA_ARGS__)
