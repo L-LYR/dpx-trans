7b27e32a9a4d14fe27b5f2197708a8c1487137c6
L-LYR <lisicen97@gmail.com>
2024-10-28 10:25:04 +0800

    fix: remote global try catch

--- sample/doca_comch_server.cxx
+++ sample/doca_comch_server.cxx
@@ -2,7 +2,7 @@
 
 #include "doca_comch_common.hxx"
 
-int main(int argc, char* argv[]) try {
+int main(int argc, char* argv[]) {
   spdlog::set_level(spdlog::level::trace);
 
   args::ArgumentParser p("Sample comch server");
@@ -32,7 +32,4 @@ int main(int argc, char* argv[]) try {
   a.associate({e}).listen_and_accept();
 
   return 0;
-} catch (const std::runtime_error& e) {
-  std::cerr << e.what() << std::endl;
-  return -2;
 }
