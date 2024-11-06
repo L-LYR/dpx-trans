6642539fcd090928ba5dd2ecb130acf56ce5f016
L-LYR <lisicen97@gmail.com>
2024-11-06 20:08:59 +0800

    fix

--- sample/doca_comch_client.cxx
+++ sample/doca_comch_client.cxx
@@ -38,7 +38,7 @@ int main(int argc, char* argv[]) {
   std::this_thread::sleep_for(5s);
 
   auto call_fn = [&]() {
-    for (auto i = 0; i < 1; i++) {
+    for (auto i = 0; i < 10000; i++) {
       auto echo_resp = t.call<EchoRpc>(payload_4k);
       auto resp = echo_resp.get();
     }
